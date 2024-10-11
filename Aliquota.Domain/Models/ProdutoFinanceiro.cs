bdd1fcb8bdaccb0ed135a07da32d742e23ba2027
luizolivas <luizolivas@gmail.com>
2024-10-11 09:46:34 -0300

    Create models

---
+++ Aliquota.Domain/Models/ProdutoFinanceiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliquota.Domain.Models
+{
+    public class ProdutoFinanceiro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Valor { get; set; }
+        public DateTime DataAplicacao { get; set; }
+    }
+}
