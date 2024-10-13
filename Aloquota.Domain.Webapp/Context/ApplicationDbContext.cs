cfeccd75b19fdd44e81fbed388db42fd3c60336d
Técnico Lift <tecnicolift@hotmail.com>
2024-10-12 21:43:41 -0300

    Add DbContext

---
+++ Aloquota.Domain.Webapp/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Aliquota.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aliquota.Domain.Webapp.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<ProdutoFinanceiro> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HistoricoMovimentacao>()
+                .HasOne(h => h.ProdutoFinanceiro)
+                .WithMany(p => p.HistoricoMovimentacoes)
+                .HasForeignKey(h => h.ProdutoFinanceiroId);
+
+            modelBuilder.Entity<ProdutoFinanceiro>()
+                .Property(p => p.Valor)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<HistoricoMovimentacao>()
+                .Property(h => h.Valor)
+                .HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
